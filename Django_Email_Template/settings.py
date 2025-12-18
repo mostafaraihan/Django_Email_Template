cb45d564c06defdaa0c53d14df90df6eeb2a7415
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-18 20:28:15 +0600

    vercel diploy ready

--- Django_Email_Template/settings.py
+++ Django_Email_Template/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%5tjj(y*o=!^5tlp6j3ivbwhm^s+#i*ht&@#hnq2s!2gqduk)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 
